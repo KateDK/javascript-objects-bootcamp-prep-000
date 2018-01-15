7b427f3b29899585f669a1448e2172c8b34ae127
Kate <kate.dubitski@gmail.com>
2018-01-15 18:01:43 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,6 +2,6 @@ var playlist ={Madonna: "Like a virgin"};
 
 
 function updatePlaylist(playlist, artistName, songTitle){
-  playlist.artistName = songTitle;
+  playlist[artistName] = songTitle;
   return playlist;
 }
